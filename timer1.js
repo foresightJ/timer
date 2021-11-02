a563db14bff6a072765a93cdedfe7180e701d444
John Foresight <foresight400@gmail.com>
2021-11-02 23:09:49 +0000

    alarm clock?

--- timer1.js
+++ timer1.js
@@ -2,16 +2,19 @@
 // "use strict"
 
 const args = process.argv.splice(2).map(x => Number(x) * 1000);
-console.log(args);
 
 const alarmClock = function(args) {
 
-  for (let ele of args) {
+  if (!args) return process.exit();
+  for (let element of args) {
+    if (element === NaN || element < 0) {
+      continue;
+    } 
+    setTimeout(() => process.stdout.write('⏰⏰'), element);
     // setTimeout(() => process.stdout.write('\x07'), ele);
-    setTimeout(() => process.stdout.write('⏰⏰'), ele);
   }
-  
-}
+};
+
 alarmClock(args);
 
 // setTimeout(() => process.stdout.write('\n'), ele + 300); 
